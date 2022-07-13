3bc961bfa43de0ad2045cde7820d42c027d61053
kiritoSlayers <fersilvavillamarin@gmail.com>
2022-07-12 21:23:43 -0300

    Enviando projeto

---
+++ Download Software Files/Forms/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Download_Software_Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.nodeJs = new System.Windows.Forms.RadioButton();
+            this.sublimeText = new System.Windows.Forms.RadioButton();
+            this.vsCode = new System.Windows.Forms.RadioButton();
+            this.git = new System.Windows.Forms.RadioButton();
+            this.discord = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnDownload.Location = new System.Drawing.Point(247, 75);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(139, 46);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download File(s)";
+            this.btnDownload.UseVisualStyleBackColor = false;
+            this.btnDownload.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nodeJs
+            // 
+            this.nodeJs.AutoSize = true;
+            this.nodeJs.Location = new System.Drawing.Point(39, 58);
+            this.nodeJs.Name = "nodeJs";
+            this.nodeJs.Size = new System.Drawing.Size(61, 17);
+            this.nodeJs.TabIndex = 1;
+            this.nodeJs.TabStop = true;
+            this.nodeJs.Text = "NodeJs";
+            this.nodeJs.UseVisualStyleBackColor = true;
+            // 
+            // sublimeText
+            // 
+            this.sublimeText.AutoSize = true;
+            this.sublimeText.Location = new System.Drawing.Point(39, 81);
+            this.sublimeText.Name = "sublimeText";
+            this.sublimeText.Size = new System.Drawing.Size(86, 17);
+            this.sublimeText.TabIndex = 2;
+            this.sublimeText.TabStop = true;
+            this.sublimeText.Text = "Sublime Text";
+            this.sublimeText.UseVisualStyleBackColor = true;
+            // 
+            // vsCode
+            // 
+            this.vsCode.AutoSize = true;
+            this.vsCode.Location = new System.Drawing.Point(39, 104);
+            this.vsCode.Name = "vsCode";
+            this.vsCode.Size = new System.Drawing.Size(81, 17);
+            this.vsCode.TabIndex = 3;
+            this.vsCode.TabStop = true;
+            this.vsCode.Text = "Visual Code";
+            this.vsCode.UseVisualStyleBackColor = true;
+            // 
+            // git
+            // 
+            this.git.AutoSize = true;
+            this.git.Location = new System.Drawing.Point(39, 127);
+            this.git.Name = "git";
+            this.git.Size = new System.Drawing.Size(58, 17);
+            this.git.TabIndex = 4;
+            this.git.TabStop = true;
+            this.git.Text = "GitHub";
+            this.git.UseVisualStyleBackColor = true;
+            // 
+            // discord
+            // 
+            this.discord.AutoSize = true;
+            this.discord.Location = new System.Drawing.Point(39, 150);
+            this.discord.Name = "discord";
+            this.discord.Size = new System.Drawing.Size(61, 17);
+            this.discord.TabIndex = 5;
+            this.discord.TabStop = true;
+            this.discord.Text = "Discord";
+            this.discord.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(398, 222);
+            this.Controls.Add(this.discord);
+            this.Controls.Add(this.git);
+            this.Controls.Add(this.vsCode);
+            this.Controls.Add(this.sublimeText);
+            this.Controls.Add(this.nodeJs);
+            this.Controls.Add(this.btnDownload);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Software Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.RadioButton nodeJs;
+        private System.Windows.Forms.RadioButton sublimeText;
+        private System.Windows.Forms.RadioButton vsCode;
+        private System.Windows.Forms.RadioButton git;
+        private System.Windows.Forms.RadioButton discord;
+    }
+}
+
---
+++ Download Software Files/Forms/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Download_Software_Files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Open web Client
+            WebClient client = new WebClient();
+
+            // Get user name
+            string userName = Environment.UserName;
+            // Get path destiny
+            string pathDestiny = $@"C:\Users\{userName}\Downloads\\";
+
+            // verify raddion button is checked
+            if (nodeJs.Checked == true)
+            {
+                // new path destiny name file and extension
+                string pathDestinyName = pathDestiny + @"\\node.msi";
+                // download this file 
+                client.DownloadFile("https://nodejs.org/dist/v16.16.0/node-v16.16.0-x64.msi", pathDestinyName);
+            }
+            if (sublimeText.Checked == true)
+            {
+                // new path destiny name file and extension
+                string pathDestenyName = pathDestiny + @"\\sublimeText.exe";
+                // download this file 
+                client.DownloadFile("https://download.sublimetext.com/Sublime%20Text%20Build%203211%20x64%20Setup.exe", pathDestenyName);
+            }
+            if (vsCode.Checked == true)
+            {
+                // new path destiny name file and extension
+                string pathDestinyName = pathDestiny + @"\\vscode.exe";
+                // download this file 
+                client.DownloadFile("https://az764295.vo.msecnd.net/stable/b06ae3b2d2dbfe28bca3134cc6be65935cdfea6a/VSCodeUserSetup-x64-1.69.1.exe", pathDestinyName);
+            }
+            if (git.Checked == true)
+            {
+                // new path destiny name file and extension
+                string pathDestinyName = pathDestiny + @"\\git.exe";
+                // download this file 
+                client.DownloadFile("https://github.com/git-for-windows/git/releases/download/v2.37.0.windows.1/Git-2.37.0-64-bit.exe", pathDestinyName);
+            }
+            if (discord.Checked == true)
+            {
+                // new path destiny name file and extension
+                string pathDestinyName = pathDestiny + @"\\Discord.exe";
+                // download this file 
+                client.DownloadFile("https://dl.discordapp.net/distro/app/stable/win/x86/1.0.9005/DiscordSetup.exe", pathDestinyName);
+            }
+
+        }
+    }
+}
